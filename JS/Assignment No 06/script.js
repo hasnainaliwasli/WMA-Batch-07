8df094b2be5526e2f3e6737090b44627a9da9368
hasnainaliwasli <hasnain62712@gmail.com>
2024-04-09 00:06:02 +0500

    Assignment No 06(Layout Complete)

---
+++ JS/Assignment No 06/script.js
@@ -0,0 +1,61 @@
+
+
+
+// 1.Round a Number
+
+function roundNum() {
+
+}
+
+
+// 2.Ceil a Number
+
+function ceil() {
+
+
+}
+
+// 3.Floor a Number
+
+function floor() {
+
+}
+
+
+// 4.Generate a random Number
+function randomNum() {
+
+}
+
+
+// 5.Throw a Dice
+function dice() {
+
+}
+
+
+// 6.Generate a Strong Password
+function strongPass() {
+
+
+}
+
+
+// 7.Converting Strings
+function convertString() {
+
+}
+
+
+// 8.Controll the Lenght
+function controlLength() {
+
+
+}
+
+
+// 9.Calculate the GST
+function gst() {
+
+
+}
