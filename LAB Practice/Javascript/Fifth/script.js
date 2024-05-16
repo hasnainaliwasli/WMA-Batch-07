4290c0f206aed15af3aae70afb34c54b5caa4f50
hasnainaliwasli <hasnain62712@gmail.com>
2024-05-16 16:09:54 +0500

    Fith Lab Practice

--- LAB Practice/Javascript/Fifth/script.js
+++ LAB Practice/Javascript/Fifth/script.js
@@ -4,12 +4,29 @@ const randomId = () => Math.random().toString(36).slice(2)
 function age() {
     let today = new Date()
     let datebirth = new Date(inputfield('dob'));
+
+    // Total age in milisecond
     let age = today - datebirth;
-    // let ageYears = Math.floor(age / (1000 * 60 * 60 * 24 * 365))
+
+    // Complete Age in Years
+    let Years = Math.floor(age / (1000 * 60 * 60 * 24 * 365))
+
+    // Complete Age in Days
     let agedays = Math.floor(age / (1000 * 60 * 60 * 24))
 
-    // return ageYears;
-    return agedays + " Days";
+    // Days left after subtracting Years
+    let daysOfYears = Years * 365;
+    let days = agedays - daysOfYears;
+
+    // Months Left after Subtracting Years
+    let months = Math.floor(days / 30)
+
+    // Days left after all calculation
+    let daysToSubtract = months * 30;
+    let daysLeft = days - daysToSubtract;
+
+    let completeAge = Years + " Years, " + months + " Months, " + daysLeft + " Days"
+    return completeAge;
     // age to days
 }
 
