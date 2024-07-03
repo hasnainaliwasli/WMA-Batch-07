47811ec7a27a8d568b9d08d9150d1d3ee958b590
hasnainaliwasli <hasnain62712@gmail.com>
2024-07-03 11:07:32 +0500

    Todo_app_JS Signup_Script_done

--- Hackathons/Todo App with JS/App.js
+++ Hackathons/Todo App with JS/App.js
@@ -30,12 +30,17 @@ function Toast(msg, cond) {
 }
 
 // For set Year in footer
-let curentDate = new Date().getFullYear()
-document.getElementById('curentDate').innerHTML = curentDate;
+let curentDate = new Date()
+document.getElementById('curentDate').innerHTML = curentDate.getFullYear();
 
 // for getting elements
-function getElement(id){
+function getElement(id) {
     return document.getElementById(id)
+}
+
+// ID Generating
+function idGenerator() {
+    return Math.random().toString(32).slice(2)
 }
 
 
@@ -56,6 +61,45 @@ signinToggle.addEventListener('click', () => {
     signin_container.classList.remove('d-none');
 })
 
+////////////////////////////////////////////////////
+// For SignUp
+let users = JSON.parse(localStorage.getItem('Users'));
+if (!users) {
+    users = []
+}
+
+function register() {
+    let signupUserName = getElement('signupUserName').value
+    let signupEmail = getElement('signupEmail').value
+    let signupPassword = getElement('signupPassword').value
+    let UserId = idGenerator();
+    let createdAT = curentDate.toLocaleDateString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit' });
+
+    if (!signupUserName, !signupEmail, !signupPassword) {
+        return Toast("Please Fillup All the Fields", 'error')
+    } else {
+
+        let user = {
+            signupUserName,
+            signupEmail,
+            signupPassword,
+            UserId,
+            createdAT
+        }
+
+        users.push(user);
+        let allUsers = JSON.stringify(users)
+        localStorage.setItem('Users', allUsers)
+        console.log(users);
+        Toast("Registration Sucessfull", 'success')
+        getElement('signupUserName').value = ''
+        getElement('signupEmail').value = ''
+        getElement('signupPassword').value = ''
+        signup_container.classList.add('d-none');
+        signin_container.classList.remove('d-none');
+
+    }
+}
 
 
 
