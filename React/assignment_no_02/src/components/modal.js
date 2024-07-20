086b17a479146d07c17dbbd021aa465a8bdcd0d7
hasnainaliwasli <hasnain62712@gmail.com>
2024-07-20 19:51:41 +0500

    React_Assignment_02

--- React/assignment_no_02/src/components/modal.js
+++ React/assignment_no_02/src/components/modal.js
@@ -1,24 +1,88 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'antd';
+import { toast, ToastContainer } from 'react-toastify';
+
+
+
+
 const App = () => {
+
+    // My created States
+    const [oldPass, setOldPass] = useState('');
+    const [newPass, setNewPass] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+
+    const [users, setUsers] = useState(() => {
+        const savedUsers = localStorage.getItem('users');
+        return savedUsers ? JSON.parse(savedUsers) : [];
+    });
+    const [Logeduser, setLogedUser] = useState(() => {
+        const savedUsers = localStorage.getItem('LoggedInUser');
+        return savedUsers ? JSON.parse(savedUsers) : [];
+    });
+
+
+
+    // Modal States
     const [open, setOpen] = useState(false);
     const [confirmLoading, setConfirmLoading] = useState(false);
-    // const [modalText, setModalText] = useState('Content of the modal');
+
+
+    //Modal Function
+
     const showModal = () => {
         setOpen(true);
     };
+
+
     const handleOk = () => {
-        // setModalText('The modal will be closed after two seconds');
+
+        // console.log("old", oldPass);
+        // console.log('new', newPass);
+        // console.log('confirm', confirmPass);
+
+
+        if (oldPass !== Logeduser.Password) {
+            return toast.error("Incorrect Old Password")
+        }
+        if (newPass !== confirmPass) {
+            toast.error("New Passwords and Confirm Password not match");
+            return;
+        }
+
+        if (newPass.length < 8) {
+            return toast.warning("Password Must be more than 7 character")
+        }
+
+
+        const updatedUsers = users.map(user => {
+            if (user.id === Logeduser.id) {
+                return { ...user, Password: newPass };
+            }
+            return user;
+        })
+        setUsers(updatedUsers);
+        localStorage.setItem('users', JSON.stringify(updatedUsers));
+        localStorage.setItem('LoggedInUser', JSON.stringify(updatedUsers));
+
+
+
+
+
+
+        // Modal Data
         setConfirmLoading(true);
         setTimeout(() => {
             setOpen(false);
             setConfirmLoading(false);
         }, 2000);
     };
+
     const handleCancel = () => {
         console.log('Clicked cancel button');
         setOpen(false);
     };
+
     return (
         <>
             <Button type="primary" onClick={showModal}>
@@ -32,14 +96,14 @@ const App = () => {
                 onCancel={handleCancel}
             >
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Old Password</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label" >Old Password</label>
                     <div className="input-group">
                         <input
                             // type={showNewPass ? "text" : "password"}
                             // value={newPass}
                             className="form-control"
                             id="exampleInputPassword1"
-                            // onChange={(e) => setNewPass(e.target.value)}
+                            onChange={(e) => setOldPass(e.target.value)}
                             required
                         />
                     </div>
@@ -47,14 +111,14 @@ const App = () => {
 
 
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">New Password</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label" >New Password</label>
                     <div className="input-group">
                         <input
                             // type={showNewPass ? "text" : "password"}
                             // value={newPass}
                             className="form-control"
                             id="exampleInputPassword1"
-                            // onChange={(e) => setNewPass(e.target.value)}
+                            onChange={(e) => { setNewPass(e.target.value) }}
                             required
                         />
                     </div>
@@ -69,7 +133,7 @@ const App = () => {
                             // value={newPass}
                             className="form-control"
                             id="exampleInputPassword1"
-                            // onChange={(e) => setNewPass(e.target.value)}
+                            onChange={(e) => { setConfirmPass(e.target.value) }}
                             required
                         />
 
@@ -85,7 +149,7 @@ const App = () => {
 
 
 
-
+                <ToastContainer />
             </Modal>
         </>
     );
